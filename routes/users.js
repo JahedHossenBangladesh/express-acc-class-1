2caf246e3615c1eb14f1845bad6c3b5f9a605b49
JahedHossenBangladesh <jahedhossen4358.com>
2020-12-23 23:53:58 +0600

    the next() work is explane in the users.js

--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,21 @@ router.get("/", function (req, res, next) {
 router.get("/id", function (req, res, next) {
   res.send("this is for id");
 });
+
+router.get(
+  "/journey",
+  function (req, res, next) {
+    console.log("1st middle ware");
+    next();
+  },
+  function (req, res, next) {
+    console.log("2nd middle ware");
+    next();
+  },
+  function (req, res, next) {
+    console.log("This is 3rd");
+    return res.send("I am all done");
+  }
+);
+
 module.exports = router;
